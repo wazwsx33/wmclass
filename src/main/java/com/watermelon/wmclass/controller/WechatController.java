5203a0a08919554a447b231e4db186b385ff20f7
wangzhenyu <717087972@qq.com>
2018-12-21 10:13:10 +0800

    实现微信登录回调

--- src/main/java/com/watermelon/wmclass/controller/WechatController.java
+++ src/main/java/com/watermelon/wmclass/controller/WechatController.java
@@ -4,6 +4,7 @@ import com.watermelon.wmclass.config.WeChatConfig;
 import com.watermelon.wmclass.domain.JsonData;
 import com.watermelon.wmclass.domain.User;
 import com.watermelon.wmclass.service.UserService;
+import com.watermelon.wmclass.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -32,7 +34,9 @@ public class WechatController {
 
     /**
      * 拼装微信扫一扫登录url
+     * @param accessPage
      * @return
+     * @throws UnsupportedEncodingException
      */
     @GetMapping(value = "login_url")
     @ResponseBody
@@ -46,12 +50,23 @@ public class WechatController {
         return JsonData.buildSuccess(qrcodeUrl);
     }
 
+    /**
+     * 微信扫码登录，回调地址
+     * @param code
+     * @param state
+     * @param response
+     * @throws IOException
+     */
     @GetMapping(value = "user/callback")
     public void wechatUserCallback(@RequestParam(value = "code", required = true) String code,
-                                   String state, HttpServletResponse response) {
+                                   String state, HttpServletResponse response) throws IOException {
         User user = userService.saveWeChatUser(code);
         if (user != null) {
             //生成jwt
+            String token = JwtUtils.geneJsonWebToken(user);
+            //state 当前用户的页面地址，需要拼接http://，这样才不会站内跳转
+            String url = state + "?token=" + token + "&head_img=" + user.getHeadImg() + "&name=" + URLEncoder.encode(user.getName(), "UTF-8");
+            response.sendRedirect(url);
         }
     }
 }
