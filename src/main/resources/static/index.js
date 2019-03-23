971d07b53545cc178b7708f2d63e7022356610ee
wangzhenyu <717087972@qq.com>
2019-03-23 17:05:15 +0800

    微信登录联调

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -27,5 +27,68 @@ $(function () {
         })
     }
 
+    //获取微信扫描地址
+    function get_wechat_login(){
+        //console.log(111)
+        //获取当前页面地址
+        var current_page = window.location.href;
+        $.ajax({
+            type:'get',
+            url: host+"/api/v1/wechat/login_url?access_page="+current_page,
+            dataType:'json',
+            success:function(res){
+                //console.info(res.data)
+                $("#login").attr("href",res.data);
+            }
+        })
+
+    }
+
+    //获取url上的参数
+    function get_params(){
+        var url = window.location.search;//获取?后面的字符串
+        var obj = new Object();
+        if(url.indexOf("?")!=-1){
+            var str = url.substr(1);
+            //console.log(str);
+            strs = str.split("&")
+            for(var i=0; i<strs.length; i++){
+                obj[strs[i].split("=")[0]] = decodeURI( strs[i].split("=")[1]);
+            }
+
+        }
+//		console.log(obj)
+        return obj;
+    }
+
+
+    //设置头像和昵称
+    function set_user_info(){
+        var user_info = get_params();
+
+        if(JSON.stringify(user_info) != '{}'){
+            //对象不为空
+            var name = user_info['name'];
+            var head_img = user_info['head_img']
+            var token = user_info['token']
+            console.log(name)
+            console.log(head_img)
+
+            $("#login").html(name)
+            $("#head_img").attr("src",head_img);
+            $.cookie('token',token,{expires:7,path:'/'})
+
+        }else{
+
+        }
+
+    }
+
+
+
+
     get_list();
+    get_wechat_login();
+    get_params();
+    set_user_info();
 })
